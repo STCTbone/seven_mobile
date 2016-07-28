f3a84c9ad63f33deca4976070255f8fa3705ef5c
Matthew Rieger <mrieger@cars.com>
2016-07-28 07:16:27 -0500

    WIP stuff.

---
+++ Weather/Weather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Matthew Rieger on 7/27/16.
+//  Copyright © 2016 Matthew Rieger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
